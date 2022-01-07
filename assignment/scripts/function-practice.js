ab0640bb26730ec7b2e615b5558ddc56b63a3612
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-07 14:58:23 -0600

    Completed stretch questions 8 & 9

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -21,7 +21,7 @@ function helloName(name) {
 } // end helloName
 
 // Remember to call the function to test
-  console.log('should say "Hello, Liz!"', helloName('Liz'));
+  console.log('should say "Hello, Liz!"', helloName('Liz')); //Output: Hello, Liz!
 
 
 // 3. Function to add two numbers together & return the result
@@ -33,8 +33,8 @@ function addNumbers(num0, num1) {
 } // end add Numbers
 
 //Test
-console.log(`The sum of 4 and 5 is:`, addNumbers(4,5));
-console.log(`The sum of 32.452 and 42.143 is:`, addNumbers(32.452,42.143));
+console.log(`The sum of 4 and 5 is:`, addNumbers(4,5)); //Output: 9
+console.log(`The sum of 32.452 and 42.143 is:`, addNumbers(32.452,42.143)); //Output: 74.595
 
 
 // 4. Function to multiply three numbers & return the result
@@ -45,9 +45,9 @@ function multiplyThree(num0, num1, num2){
 } //end multiplyThre1
 
 //Test
-console.log(`The product of 1 * 1 * 1 is:`, multiplyThree(1,1,1));
-console.log(`The product of 2 * 6 * 4 is:`, multiplyThree(2,6,4));
-console.log(`The product of 12 * 4 * 198 is:`, multiplyThree(12,4,198));
+console.log(`The product of 1 * 1 * 1 is:`, multiplyThree(1,1,1)); //Output: 1
+console.log(`The product of 2 * 6 * 4 is:`, multiplyThree(2,6,4)); //Output: 48
+console.log(`The product of 12 * 4 * 198 is:`, multiplyThree(12,4,198)); //Output: 9504
 
 
 // 5. Function that will return true if a number is positive,
@@ -62,14 +62,14 @@ function isPositive( number ) {
 } // end isPositive
 
 //Test
-console.log('The number 4 is positive:', isPositive(4));
-console.log('The number -1 is positive:', isPositive(-1));
+console.log('The number 4 is positive:', isPositive(4)); //Output: true
+console.log('The number -1 is positive:', isPositive(-1)); //Output: false
 
 // Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
-console.log( 'isPositive - should say false', isPositive(-3) );
+console.log( 'isPositive - should say true', isPositive(3) ); //Output: true
+console.log( 'isPositive - should say false', isPositive(0) ); //Output: false
+console.log( 'isPositive - should say false', isPositive(-3) ); //Output: false
 
 
 // 6. Function to return the _last_ item in an array. If the
@@ -84,10 +84,10 @@ function getLast(array) {
 } // end getLast
 
 //Test
-console.log('The last value in the array is:', getLast([3,4,4,2]));
-console.log('The last value in the array is:', getLast(['Apples', 'Oranges', 'Banana', 'Blueberries']));
-console.log('The last value in the array is:', getLast([]));
-console.log('The last value in the array is:', getLast([81, 34, 243, 13432, 123421, 493, 3245, 4596.1]));
+console.log('The last value in the array is:', getLast([3,4,4,2])); //Output: 2
+console.log('The last value in the array is:', getLast(['Apples', 'Oranges', 'Banana', 'Blueberries'])); //Output: Blueberries
+console.log('The last value in the array is:', getLast([])); //Output: undefined
+console.log('The last value in the array is:', getLast([81, 34, 243, 13432, 123421, 493, 3245, 4596.1])); //Output: 4596.1
 
 
 // 7. Function to find a value in an array. Return true if the
@@ -103,10 +103,10 @@ function find( value, array ){
 } // end find
 
 //Test
-console.log('Is the number 8 within this array?', find(8, [3, 4, 5, 6, 7, 8, 3, 1]));
-console.log('Is the number 8 within this array?', find(8, [3, 4, 3, 1]));
-console.log('Is the number Jamie in the class?', find('Jamie', ['Rob', 'Charlie', 'Selena', 'Jamie', 'Calvin']));
-console.log('Is the number Jamie in the class?', find('Jamie', ['Rob', 'Charlie', 'Selena', 'Calvin']));
+console.log('Is the number 8 within this array?', find(8, [3, 4, 5, 6, 7, 8, 3, 1])); //Output: true
+console.log('Is the number 8 within this array?', find(8, [3, 4, 3, 1])); //Output: false
+console.log('Is the number Jamie in the class?', find('Jamie', ['Rob', 'Charlie', 'Selena', 'Jamie', 'Calvin'])); //Output: true
+console.log('Is the number Jamie in the class?', find('Jamie', ['Rob', 'Charlie', 'Selena', 'Calvin'])); //Output: false
 
 // ----------------------
 // Stretch Goals
@@ -115,25 +115,56 @@ console.log('Is the number Jamie in the class?', find('Jamie', ['Rob', 'Charlie'
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
   console.log('in isFirstLetter');
-  for (let i=; i<letter.length; i++)
+  if (letter.charAt(0) === string.charAt(0)) {
+    return true; //First letters are the same
+  } else {
+    return false; //First letters are not the same
+  }
 } // end isFirstLetter
 
 //Test
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') ); //Output: true
+console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') ); //Output: false
+
+// 8b. Third letter test (**EXTRA PRACTICE**)
+function isThirdLetter(letter, string) {
+  console.log('in isThirdLetter');
+  if (letter.charAt(0) === string.charAt(2)) {
+    return true; // Third letters are the same
+  } else {
+    return false; // Third letters are not the same
+  }
+} // end isThirdLetter
+
+//Test
+console.log( 'isThirdLetter - should say true', isThirdLetter('l', 'Ralph') ); //Output: true
+console.log( 'isThirdLetter - should say false', isThirdLetter('z', 'apple') ); //Output: false
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
-  // TODO: loop to add items
+function sumAll(array) {
+  console.log('in sumAll');
+  let sum = 0;
+  for (let i=0; i<array.length; i++) {
+    sum+=array[i];
+  } // TODO: loop to add items
   return sum;
-}
+} // end sumAll
+
+//Test
+console.log('Sum of all items within array:', sumAll([4, 5, 6, 7])); //Output: 22
+console.log('Sum of all items within array:', sumAll([234, 23566, 23677, 346734])); //Output: 394211
+
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
+function positiveNumbers(array) {
+  console.log('in positiveNumbers');
 
+} // end positiveNumbers
+
+console.log('The new array is:', positiveNumbers([-3, -1, -4, -1, -2]) );
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
